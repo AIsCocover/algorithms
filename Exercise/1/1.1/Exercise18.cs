84b773962176142c0f7103d1e07ad56bb110c270
ccovor <aiccovor@gmail.com>
2018-04-15 15:48:54 +0800

    1.1.18 Realization multiply by add, exponentiation by multiply.

--- Exercise/1/1.1/Exercise18.cs
+++ Exercise/1/1.1/Exercise18.cs
@@ -10,9 +10,21 @@ public class Exercise18
 	/// <returns></returns>
 	public static int Multiply(int a, int b)
 	{
-		Console.WriteLine(a + " " + b);
 		if (b == 0) return 0;
 		if (b % 2 == 0) return Multiply(a + a, b / 2);
 		return Multiply(a + a, b / 2) + a;
+	}
+	
+	/// <summary>
+	/// Realization exponentiation by multiply. a is base, b is index.
+	/// </summary>
+	/// <param name="a"></param>
+	/// <param name="b"></param>
+	/// <returns></returns>
+	public static int Exponentiation(int a, int b)
+	{
+		if (b == 0) return 1;
+		if (b % 2 == 0) return Exponentiation(a * a, b / 2);
+		return Exponentiation(a * a, b / 2) * a;
 	}
 }
