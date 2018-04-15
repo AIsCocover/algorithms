ed1293d145e60cb1485513c5ab3f9b67fb15ba74
ccovor <aiccovor@gmail.com>
2018-04-15 15:54:53 +0800

    1.1.6 Print Fibonacci Sequence.

---
+++ Exercise/1/1.1/Exercise06.cs
@@ -0,0 +1,26 @@
+using System;
+
+public class Exercise06
+{
+	/// <summary>
+	/// Fibonacci Sequence.
+	/// </summary>
+	/// <param name="n"></param>
+	public static void FibonacciSequence(int n)
+	{
+		int f = 0;
+		int g = 1;
+		Console.WriteLine("Fibonacci Sequenct(" + n + "): ");
+		for (int i = 0; i <=n; i++)
+		{
+			if(i==n)
+			{
+				Console.WriteLine(f);
+				break;
+			}
+			Console.Write(f + " ");
+			f = f + g;
+			g = f - g;
+		}
+	}
+}
